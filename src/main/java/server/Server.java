613c704ca0020d998954a169a76d7967820ead02
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-21 14:30:08 -0700

    refactor server to support concurrency so multiple connections can be supported at once

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -2,18 +2,22 @@ package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     private Integer port;
 
-    public Server(Integer port) throws IOException {
+    public Server(Integer port) {
         this.port = port;
     }
 
     public void start() throws IOException {
         ServerSocket socket = new ServerSocket(port);
-        Client client = new Client(socket.accept());
-        HTTProtocol protocol = new HTTProtocol(client);
-        protocol.run();
+        while(true) {
+            Socket clientSocket = socket.accept();
+            Client client = new Client(clientSocket);
+            HTTProtocol protocol = new HTTProtocol(client);
+            (new Thread(protocol)).start();
+        }
     }
 }
