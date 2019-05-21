9c6bec9b7529f850c5f5863d32cae14912e7e199
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-21 11:02:06 -0700

    create main entry point into program

---
+++ src/main/java/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        Server server = new Server(port);
+        server.start();
+    }
+}
