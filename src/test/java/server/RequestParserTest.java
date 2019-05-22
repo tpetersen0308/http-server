83ae0c6e87869b476aa73ef5968f1ed6a3010312
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-22 15:14:57 -0700

    extract method to parse the request line from the input stream

--- src/test/java/server/RequestParserTest.java
+++ src/test/java/server/RequestParserTest.java
@@ -1,9 +1,9 @@
 package server;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import server.stubs.SocketStub;
 
@@ -12,29 +12,50 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class RequestParserTest {
+    String request;
+    String requestLine;
+    String requestBody;
+    SocketStub socket;
+    BufferedReader in;
+    RequestParser parser;
+
+    @Before
+    public void stubRequest() {
+        requestLine = "GET /redirect HTTP/1.1";
+        String requestHeaders = "User-Agent: Ruby\n" + "Connection: close\n" + "Host: 127.0.0.1:5000\n" + "Content-Length: 46";
+        requestBody = "lorem ipsum dolor sit amet, adipiscing elit...";
+        request = requestLine + "\r\n" + requestHeaders + "\n\n" + requestBody;
+    }
+
+    @Before
+    public void setupInputStreamReader() {
+        socket = new SocketStub(request);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    @Before
+    public void setupParser() {
+        parser = new RequestParser();
+    }
+
     @Test
     public void shouldReturnEntireParsedRequest() {
-        String requestLine = "GET /redirect HTTP/1.1";
-        String requestHeaders = "User-Agent: Ruby\n" + "Connection: close\n" + "Host: 127.0.0.1:5000\n" + "Content-Length: 9";
-        String requestBody = "some body"; //"lorem ipsum dolor sit amet, adipiscing elit...";
-        String request = requestLine + "\r\n" + requestHeaders + "\n\n" + requestBody;
-
         HashMap headersMap = new HashMap<String, String>();
         headersMap.put("User-Agent", "Ruby");
         headersMap.put("Connection", "close");
         headersMap.put("Host", "127.0.0.1:5000");
-        headersMap.put("Content-Length", "9");
+        headersMap.put("Content-Length", "46");
 
         HashMap expectedResult = new HashMap<>();
         expectedResult.put("requestLine", requestLine);
         expectedResult.put("requestHeaders", headersMap);
         expectedResult.put("requestBody", requestBody);
 
-
-        SocketStub socket = new SocketStub(request);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        RequestParser parser = new RequestParser();
-
         assertEquals(expectedResult, parser.parse(in));
+    }
+
+    @Test
+    public void shouldParseRequestLine() {
+        assertEquals("GET /redirect HTTP/1.1", parser.parseRequestLine(in));
     }
 }
