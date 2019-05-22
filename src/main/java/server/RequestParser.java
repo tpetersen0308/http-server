74dc0ff136315bd329b234a66a41e32cba0ed824
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-22 14:33:19 -0700

    add class to parse requests

---
+++ src/main/java/server/RequestParser.java
@@ -0,0 +1,52 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class RequestParser {
+    private BufferedReader in;
+
+    public RequestParser(BufferedReader in) {
+        this.in = in;
+    }
+
+    public HashMap parse() {
+        HashMap parsedRequest = new HashMap();
+        try {
+            String requestLine = in.readLine();
+            parsedRequest.put("requestLine", requestLine);
+
+            HashMap<String, String> requestHeaders = new HashMap<>();
+            String header = in.readLine();
+            if(header != null) {
+                while(!header.isEmpty()) {
+                    String[] headerComponents = header.split(": ");
+                    requestHeaders.put(headerComponents[0].trim(), headerComponents[1].trim());
+                    header = in.readLine();
+                }
+
+                parsedRequest.put("requestHeaders", requestHeaders);
+            } else {
+                parsedRequest.put("requestHeaders", "");
+            }
+
+            StringBuilder requestBody = new StringBuilder();
+            int nextBodyChar;
+
+            String contentLengthStr = requestHeaders.get("Content-Length");
+            Integer contentLengthInt = contentLengthStr == null ? 0 : Integer.parseInt(contentLengthStr);
+
+            for(int i = 0; i < contentLengthInt; i++) {
+                nextBodyChar = in.read();
+                requestBody.append((char)nextBodyChar);
+            }
+
+            parsedRequest.put("requestBody", requestBody.toString());
+
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return parsedRequest;
+    }
+}
