8b057c5c8a325c0b745902984e386565b441a938
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-22 14:35:05 -0700

    add 200 OK HTTP response

--- src/main/java/server/StatusCodes.java
+++ src/main/java/server/StatusCodes.java
@@ -4,4 +4,5 @@ public class StatusCodes {
     private StatusCodes() {}
 
     public static final String NOT_FOUND = "HTTP/1.1 404 Not Found\r\n";
+    public static final String OK = "HTTP/1.1 200 OK\r\n";
 }
