ca6f557358e23c7d3dbbb0209cb801bfec981d8f
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-23 09:44:02 -0700

    extract routes into separate class

--- src/main/java/server/HTTProtocol.java
+++ src/main/java/server/HTTProtocol.java
@@ -2,10 +2,12 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import static server.StatusCodes.OK;
 import static server.StatusCodes.NOT_FOUND;
+import static server.Routes.ROUTES;
 
 public class HTTProtocol implements Runnable {
     private Client client;
@@ -22,8 +24,9 @@ public class HTTProtocol implements Runnable {
         RequestParser parser = new RequestParser();
         HashMap parsedRequest = parser.parse(in);
         String requestLine = (String)parsedRequest.get("requestLine");
+        String requestPath = requestLine.split(" ")[1].trim();
 
-        if(requestLine.contains("/simple_get") || requestLine.contains("/get_with_body")){
+        if(Arrays.asList(ROUTES).contains(requestPath)){
             out.print(OK);
         } else {
             out.print(NOT_FOUND);
---
+++ src/main/java/server/Routes.java
@@ -0,0 +1,9 @@
+package server;
+
+import java.util.HashMap;
+
+public class Routes {
+    private Routes() {};
+
+    public static String[] ROUTES = {"/simple_get", "/get_with_body"};
+}
