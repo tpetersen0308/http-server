35415074b883ca7caa2416602679a274a78888e4
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-28 16:32:32 -0700

    make socket field private

--- src/main/java/server/Client.java
+++ src/main/java/server/Client.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Client {
-    Socket socket;
+    private Socket socket;
     private BufferedReader inputStreamReader;
     private PrintWriter outputStreamWriter;
 
