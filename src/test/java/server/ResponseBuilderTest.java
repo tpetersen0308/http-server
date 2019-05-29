e77700d160b8eb20e732b56fc71f5cec0f4e4683
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-29 13:48:36 -0700

    refactor to test response as a data structure that cannot parse its own data

--- src/test/java/server/ResponseBuilderTest.java
+++ src/test/java/server/ResponseBuilderTest.java
@@ -11,11 +11,12 @@ import java.util.HashMap;
 
 public class ResponseBuilderTest {
     @Test
-    public void shouldReturnResponseWithOKStatusLine() {
+    public void shouldReturnResponseWithOKStatus() {
         Request request = new Request("GET /simple_get HTTP/1.1", "/simple_get", new HashMap<String, String>(), "");
         Response response = ResponseBuilder.buildResponse(request);
 
-        assertEquals("HTTP/1.1 200 OK\r\n\r\n", response.statusLine());
+        assertEquals("200", response.statusCode());
+        assertEquals("OK", response.reasonPhrase());
     }
 
     @Test
@@ -23,6 +24,15 @@ public class ResponseBuilderTest {
         Request request = new Request("GET /not_found_resource HTTP/1.1", "/not_found_resource", new HashMap<String, String>(), "");
         Response response = ResponseBuilder.buildResponse(request);
 
-        assertEquals("HTTP/1.1 404 Not Found\r\n\r\n", response.statusLine());
+        assertEquals("404", response.statusCode());
+        assertEquals("Not Found", response.reasonPhrase());
+    }
+
+    @Test
+    public void shouldReturnResponseWithHeaders() {
+        Request request = new Request("OPTIONS /method_options HTTP/1.1", "/method_options", new HashMap<String, String>(), "");
+        Response response = ResponseBuilder.buildResponse(request);
+
+        assertEquals("GET, HEAD, OPTIONS", response.headers().get("Allow"));
     }
 }
