937ef0531e05ec310a26e2e09c3ab32d7e7d6ce2
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-29 14:05:36 -0700

    remove unnecessary request line parameter from request class

--- src/test/java/server/ResponseBuilderTest.java
+++ src/test/java/server/ResponseBuilderTest.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 public class ResponseBuilderTest {
     @Test
     public void shouldReturnResponseWithOKStatus() {
-        Request request = new Request("GET /simple_get HTTP/1.1", "/simple_get", new HashMap<String, String>(), "");
+        Request request = new Request("/simple_get", new HashMap<String, String>(), "");
         Response response = ResponseBuilder.buildResponse(request);
 
         assertEquals("200", response.statusCode());
@@ -21,7 +21,7 @@ public class ResponseBuilderTest {
 
     @Test
     public void shouldReturnResponseWithNotFoundStatusLine() {
-        Request request = new Request("GET /not_found_resource HTTP/1.1", "/not_found_resource", new HashMap<String, String>(), "");
+        Request request = new Request("/not_found_resource", new HashMap<String, String>(), "");
         Response response = ResponseBuilder.buildResponse(request);
 
         assertEquals("404", response.statusCode());
@@ -30,7 +30,7 @@ public class ResponseBuilderTest {
 
     @Test
     public void shouldReturnResponseWithHeaders() {
-        Request request = new Request("OPTIONS /method_options HTTP/1.1", "/method_options", new HashMap<String, String>(), "");
+        Request request = new Request("/method_options", new HashMap<String, String>(), "");
         Response response = ResponseBuilder.buildResponse(request);
 
         assertEquals("GET, HEAD, OPTIONS", response.headers().get("Allow"));
