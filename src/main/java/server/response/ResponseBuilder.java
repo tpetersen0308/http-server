cfa0b7bfc5bf1a2123201d9eb07b1ce19d07473c
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-29 13:46:41 -0700

    refactor to include headers in response

--- src/main/java/server/response/ResponseBuilder.java
+++ src/main/java/server/response/ResponseBuilder.java
@@ -2,6 +2,7 @@ package server.response;
 
 import server.request.Request;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static server.Routes.ROUTES;
@@ -9,10 +10,17 @@ import static server.Routes.ROUTES;
 public class ResponseBuilder {
     public static Response buildResponse(Request request) {
         Map<String, String> route = ROUTES.get(request.path());
+
         if (route != null) {
-            return new Response(StatusCodes.OK, ReasonPhrases.OK);
+            return new Response(StatusCodes.OK, ReasonPhrases.OK, buildHeaders(route));
         } else {
-            return new Response(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND);
+            return new Response(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND, new HashMap<String, String>());
         }
+    }
+
+    private static Map<String, String> buildHeaders(Map<String, String> route) {
+        Map<String, String> headers = new HashMap();
+        headers.put("Allow", route.get("methods"));
+        return headers;
     }
 }
