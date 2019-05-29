dd901da9fd5c6654eb67dc85a9323c993f8da097
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-29 12:35:15 -0700

    refactor such that routes are stored in a map with allowed methods

--- src/main/java/server/Routes.java
+++ src/main/java/server/Routes.java
@@ -1,8 +1,18 @@
 package server;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Routes {
     private Routes() {
     }
 
-    public static String[] ROUTES = {"/simple_get", "/get_with_body"};
+    public static Map<String, Map<String, String>> ROUTES = new HashMap() {{
+        put("/simple_get", new HashMap() {{
+            put("methods", "GET, HEAD");
+        }});
+        put("/get_with_body", new HashMap() {{
+            put("methods", "GET, HEAD");
+        }});
+    }};
 }
--- src/main/java/server/response/ResponseBuilder.java
+++ src/main/java/server/response/ResponseBuilder.java
@@ -2,13 +2,14 @@ package server.response;
 
 import server.request.Request;
 
-import java.util.Arrays;
+import java.util.Map;
 
 import static server.Routes.ROUTES;
 
 public class ResponseBuilder {
     public static Response buildResponse(Request request) {
-        if (Arrays.asList(ROUTES).contains(request.path())) {
+        Map<String, String> route = ROUTES.get(request.path());
+        if (route != null) {
             return new Response(StatusCodes.OK, ReasonPhrases.OK);
         } else {
             return new Response(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND);
