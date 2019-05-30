157a9d26435649f06140ea954c7776855d70bf47
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-30 14:54:14 -0700

    refactor to parse body

--- src/main/java/server/response/ResponseParser.java
+++ src/main/java/server/response/ResponseParser.java
@@ -3,14 +3,18 @@ package server.response;
 public class ResponseParser {
 
     public static String parse(Response response) {
-        return parseStatusLine(response) + parseHeaders(response);
+        return parseStatusLine(response)+ ResponseComponents.CRLF + parseHeaders(response) + ResponseComponents.CRLF + response.body();
     }
 
     private static String parseStatusLine(Response response) {
-        return ResponseComponents.HTTP_VERSION + ResponseComponents.SP + response.statusCode() + ResponseComponents.SP + response.reasonPhrase() + ResponseComponents.CRLF;
+        return ResponseComponents.HTTP_VERSION + ResponseComponents.SP + response.statusCode() + ResponseComponents.SP + response.reasonPhrase();
     }
 
     private static String parseHeaders(Response response) {
-        return response.headers().toString().replace("{", "").replace("}", "").replace("=", ": ") + ResponseComponents.CRLF;
+        if(response.headers().size() > 0) {
+            return response.headers().toString().replace("{", "").replace("}", "").replace("=", ": ") + ResponseComponents.CRLF;
+        } else {
+            return "";
+        }
     }
 }
