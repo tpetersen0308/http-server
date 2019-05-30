733b0e50766966191be24f15f01ac833a41d8d67
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-30 14:34:17 -0700

    specify hashmap types and add route for /echo_body to satisfy acceptance criteria for handling a POST request

--- src/main/java/server/Routes.java
+++ src/main/java/server/Routes.java
@@ -7,15 +7,18 @@ public class Routes {
     private Routes() {
     }
 
-    public static Map<String, Map<String, String>> ROUTES = new HashMap() {{
-        put("/simple_get", new HashMap() {{
+    public static Map<String, Map<String, String>> ROUTES = new HashMap<String, Map<String, String>>() {{
+        put("/simple_get", new HashMap<String, String>() {{
             put("methods", "GET, HEAD");
         }});
-        put("/get_with_body", new HashMap() {{
+        put("/get_with_body", new HashMap<String, String>() {{
             put("methods", "GET, HEAD");
         }});
-        put("/method_options", new HashMap() {{
+        put("/method_options", new HashMap<String, String>() {{
             put("methods", "GET, HEAD, OPTIONS");
+        }});
+        put("/echo_body", new HashMap<String, String>() {{
+            put("methods", "GET, HEAD, OPTIONS, POST");
         }});
     }};
 }
