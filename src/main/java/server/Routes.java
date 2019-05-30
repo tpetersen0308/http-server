a4a5923cf2d12863471365ec649dfcd3ffd64b76
tpetersen0308 <travis.e.petersen@gmail.com>
2019-05-30 15:21:43 -0700

    refactor routes as unmodifiable map

--- src/main/java/server/Routes.java
+++ src/main/java/server/Routes.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,18 +8,24 @@ public class Routes {
     private Routes() {
     }
 
-    public static Map<String, Map<String, String>> ROUTES = new HashMap<String, Map<String, String>>() {{
-        put("/simple_get", new HashMap<String, String>() {{
-            put("methods", "GET, HEAD");
-        }});
-        put("/get_with_body", new HashMap<String, String>() {{
-            put("methods", "GET, HEAD");
-        }});
-        put("/method_options", new HashMap<String, String>() {{
-            put("methods", "GET, HEAD, OPTIONS");
-        }});
-        put("/echo_body", new HashMap<String, String>() {{
-            put("methods", "GET, HEAD, OPTIONS, POST");
-        }});
-    }};
+    public static final Map<String, Map<String, String>> ROUTES;
+    static {
+
+        Map<String, Map<String, String>> routes = new HashMap<String, Map<String, String>>() {{
+            put("/simple_get", new HashMap<String, String>() {{
+                put("methods", "GET, HEAD");
+            }});
+            put("/get_with_body", new HashMap<String, String>() {{
+                put("methods", "GET, HEAD");
+            }});
+            put("/method_options", new HashMap<String, String>() {{
+                put("methods", "GET, HEAD, OPTIONS");
+            }});
+            put("/echo_body", new HashMap<String, String>() {{
+                put("methods", "GET, HEAD, OPTIONS, POST");
+            }});
+        }};
+
+        ROUTES = Collections.unmodifiableMap(routes);
+    }
 }
